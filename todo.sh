82fbf032b0f8bf5c44e68b2f4bfa18173c034221
Xie Benyi <xieby1@outlook.com>
2020-08-31 20:30:23 +0800

    chores

--- todo.sh
+++ todo.sh
@@ -7,22 +7,34 @@
 source ~/Documents/shell-scripts/_common.sh
 source ~/Documents/shell-scripts/_grep_common.sh
 
+#### Local Variables ####
 TODO_OPTION=(-i -E -w)
 BAT_STYLE=--style="grid,numbers"
+EXCLUDE_TODO=(
+    --exclude="*todo.txt"
+    --exclude="*done.txt"
+    --exclude-dir="CrossPlatformTools"
+)
+GREP_OPTIONS=(
+    . -r
+    ${EXTRA_OPTION[@]}
+    ${TODO_OPTION[@]}
+    ${EXCLUDE_BASIC[@]}
+    ${EXCLUDE_CODE[@]}
+    ${EXCLUDE_TODO[@]}
+)
+#### End of Local Variables ####
 
-PATTERN="(todo)|(\[open\])"
-LOCATION=~/Documents/Notes/xiebenyi
-cd "${LOCATION}"
+cd ~/Documents/Notes/xiebenyi
 head_line Notes
-grep "${PATTERN}" . -r \
-    ${EXTRA_OPTION[@]} \
-    ${TODO_OPTION[@]} \
-    ${EXCLUDE_BASIC[@]} \
-    ${EXCLUDE_CODE[@]} \
-    | bat -l md "${BAT_STYLE}"
+grep "todo" ${GREP_OPTIONS[@]} \
+    | bat "${BAT_STYLE}"
+grep "\[open\]" ${GREP_OPTIONS[@]} \
+    | bat "${BAT_STYLE}"
+
 
 head_line todo.txt
-bat "${BAT_STYLE}" "${LOCATION}/todo.txt"
+bat "${BAT_STYLE}" "./todo.txt"
 
 PATTERN="W.*"
 LOCATION=~/Documents/Essays
@@ -30,5 +42,5 @@ cd "${LOCATION}"
 head_line Essays
 find -iname "${PATTERN}" \
     | bat "${BAT_STYLE}"
-
-
+grep "todo" ${GREP_OPTIONS[@]} \
+    | bat "${BAT_STYLE}"
