8a7c8813432dfd60999918c208a2907c4a5a5c16
Xie Benyi <xieby1@outlook.com>
2020-09-09 17:42:01 +0800

    feat: ldd-grep.sh: list dependencies recursively

--- ldd-grep.sh
+++ ldd-grep.sh
@@ -1,4 +1,33 @@
 #!/bin/bash
-# input the file to be examed, output its related lib
-list="$(ldd "$1" | grep -E -o '\/lib.*\.[0-9]+')"
-echo "${list}"
+# input: the file (or diretory) to be examed,
+# output: its (or files under the directory, recursively) depending lib
+for opt in $@; do
+    if [[ ${opt} == "-h" || ${opt} == "--help" ]]; then
+        echo "Usage: ${0##*/} [ file | directory ]"
+        echo
+        echo "  List the lib dependencies of file or files under directory"
+        echo "  If file or directory is not specified, '.' will be used."
+        exit 0
+    fi
+done
+
+ldd_real_path()
+{
+    ldd "$1" 2> /dev/null |\
+        sed 's/.*=>//' |\
+        grep -E -o '\/.*\s' |\
+        sed 's/\s//'
+}
+
+TARGET=.
+if [[ $# -ge 1 ]]; then
+    TARGET=$1
+fi
+
+if [[ -d ${TARGET} ]]; then
+    for file in $(find .); do
+        ldd_real_path ${file}
+    done
+elif [[ -f ${TARGET} ]]; then
+    ldd_real_path $1
+fi
