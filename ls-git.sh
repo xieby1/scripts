ed51df90398b4532018cd2c6cf5888e3b762ef00
Xie Benyi <xieby1@outlook.com>
2020-09-29 17:24:08 +0800

    new file: ls-git.sh to ls files in git and with ls flavor!

---
+++ ls-git.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# ls files & directories only contaion in git repo
+FILES_IN_GIT=$(git ls-files .)
+if [[ $? -ne 0 ]]; then
+    exit $?
+fi
+echo ${FILES_IN_GIT} | \
+    sed s,/.*,, | \
+    uniq | \
+    xargs ls -d --color=auto $*
