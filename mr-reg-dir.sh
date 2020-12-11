49bfd1b3c0c2a15ddeb7e6ac899db72a2d5557f7
Xie Benyi <xieby1@outlook.com>
2020-12-11 15:38:28 +0800

    mr-reg-dir color support, optimize code

--- mr-reg-dir.sh
+++ mr-reg-dir.sh
@@ -1,22 +1,41 @@
 #!/bin/bash
 # Color according to https://stackoverflow.com/questions/5947742/how-to-change-the-output-color-of-echo-in-linux
+
+# const value
 RED="\033[0;31m"
 CYAN="\033[0;36m"
+WHITE="\033[0;37m"
 NC="\033[0m"
+#default value
 DIR=./
+
 if [[ -n $1 ]]; then
+    if [[ "$1" == "-h" ]]; then
+        echo "Usage: ${0##*/} [DIR]"
+        echo
+        echo "  mr register all repos in DIR into mrconfig file"
+        echo "  if DIR is not specified, '.' will be used"
+        echo "  the default mrconfig is ~/.mrconfig"
+        exit 0;
+    fi
     DIR=$1
 fi
-ls "${DIR}" | while read REPO; do
+
+for REPO in ${DIR}/*; do
     if [[ -d "${REPO}" ]]; then # if this REPO is a directory
-        mr register "${REPO}" > /dev/null
-        #if   [[ $? -eq 1 ]]; then # cannot determine url
-        #    echo "${RED}${REPO}${NC}"
-        #elif [[ $? -eq 2 ]]; then # unknown repo
-        #    ehcp "${CYAN}${REPO}${NC}"
-        #fi
-        if [[ $? -ne 0 ]]; then
-            echo "${REPO}"
-        fi
+        mr register "${REPO}" &> /dev/null
+        case $? in
+            0)
+                ;;
+            1)
+                echo -e "cannot determine url: ${RED}${REPO}${NC}"
+                ;;
+            2)
+                echo -e "unknown repo: ${CYAN}${REPO}${NC}"
+                ;;
+            *)
+                echo -e "unknown return num $?: ${WHITE}${REPO}${NC}"
+                ;;
+        esac
     fi
 done
