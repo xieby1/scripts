2f409825904d1837038063e51844d4cb70d055a5
Xie Benyi <xieby1@outlook.com>
2021-01-19 14:25:31 +0800

    mdrevealjs.sh: change reveal.js path to relative path

--- mdrevealjs.sh
+++ mdrevealjs.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
-REVEAL_URL=~/Documents/Notes/xiebenyi/CrossPlatformTools/reveal.js
+ABS_REVEAL_URL=~/Documents/Notes/xiebenyi/CrossPlatformTools/reveal.js
 TEMPLATE=~/Documents/Notes/xiebenyi/CrossPlatformTools/pandoc_revealjs.template
+
+ABS_MD_PATH=$(realpath $1)
+REL_REVEAL_URL=$(realpath --relative-to="${ABS_MD_PATH%/*}" "${ABS_REVEAL_URL}")
 
 pandoc \
     -t revealjs \
     -s \
     $1 \
     -o ${1%.*}.html \
-    -V revealjs-url=${REVEAL_URL} \
+    -V revealjs-url=${REL_REVEAL_URL} \
     --template=${TEMPLATE}
